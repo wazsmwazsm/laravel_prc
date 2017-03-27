8d061dadb11d546b8c6eb969a41f3d60f6a92ca3
shuguang <shuguang@shuguang.com>
2017-03-27 15:21:20 +0800

    Finish login and logout

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\Controller;
 
 use App\Models\User;
 
+use Auth;
+
 class UsersController extends Controller
 {
 
@@ -33,6 +35,9 @@ class UsersController extends Controller
         'email' => $request->email,
         'password' => bcrypt($request->password),
       ]);
+
+      // 注册后自动登录
+      Auth::login($user);
 
       session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
       return redirect()->route('users.show', [$user]);
