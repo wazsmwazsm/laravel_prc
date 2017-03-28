daaaa06450e09d8b266e2e4e15229ec1b35a3dea
shuguang <shuguang@shuguang.com>
2017-03-28 20:05:40 +0800

    Finish user crud

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,16 +14,31 @@ use Auth;
 class UsersController extends Controller
 {
 
-    public function create(){
+    public function __construct(){
+      $this->middleware('auth', [
+        'only' => ['edit', 'update', 'destroy']
+      ]);
+
+      $this->middleware('guest', [
+        'only' => ['create']
+      ]);
+    }
+
+    public function index(){
+      $users = User::paginate(20);
+      return view('users.index', compact('users'));
+    }
+
+    public function create() {
       return view('users.create');
     }
 
-    public function show($id){
+    public function show($id) {
       $user = User::findOrFail($id);
       return view('users.show', compact('user'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request) {
       $this->validate($request, [
         'name' => 'required|max:50',
         'email' => 'required|email|unique:users|max:255',
@@ -42,4 +57,41 @@ class UsersController extends Controller
       session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
       return redirect()->route('users.show', [$user]);
     }
+
+    public function edit($id) {
+        $user = User::findOrFail($id);
+
+        return view('users.edit', compact('user'));
+    }
+
+    public function update($id, Request $request) {
+      $this->validate($request, [
+        'name' => 'required|max:50',
+        'password' => 'confirmed|min:6',
+      ]);
+
+      $user = User::findOrFail($id);
+      $this->authorize('update', $user);
+
+      $data = [];
+      $data['name'] = $request->name;
+      if($request->password) {
+        $data['password'] = bcrypt($request->password);
+      }
+      $user->update($data);
+
+      session()->flash('success', '个人资料更新成功！');
+
+      return redirect()->route('users.show', $id);
+    }
+
+    public function destroy($id){
+      $user = User::findOrFail($id);
+      $this->authorize('destroy', $user);
+      $user->delete();
+      session()->flash('success', '成功删除用户！');
+
+      return back();
+    }
+
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(200)->make();
+        User::insert($users->toArray());
+
+        $user = User::find(1);
+        $user->name = '曙光';
+        $user->email = 'shuguang@sg.com';
+        $user->password = bcrypt('shuguang');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
