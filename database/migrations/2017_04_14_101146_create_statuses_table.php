d538269cc2cc29cba3ac5e553f8d3e951a45e17d
shuguang <shuguang@shuguang.com>
2017-04-14 19:23:56 +0800

    create status model

---
+++ database/migrations/2017_04_14_101146_create_statuses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
+            // 自动生成时间戳维护字段
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('statuses');
+    }
+}
